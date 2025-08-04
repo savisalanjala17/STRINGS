ddf8f6225bae716ad44d3b4c9a22a9124d6e506c
savisalanjala17 <savisalanjala17@gmail.com>
2025-08-04 19:33:58 +0530

    Create 4.java

---
+++ LEETCODE/HASHING/4.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+
+        HashSet<Character> h = new HashSet<>();
+
+        for(char c : sentence.toCharArray()){
+            h.add(c);
+        }
+
+        if (h.size() == 26){
+
+            return true;
+
+        }
+
+        return false;
+
+        
+    }
+}
+
+
+1832. Check if the Sentence Is Pangram
+
+Hint
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+Example 2:
+
+Input: sentence = "leetcode"
+Output: false
+ 
+
+Constraints:
+
+1 <= sentence.length <= 1000
+sentence consists of lowercase English letters.
