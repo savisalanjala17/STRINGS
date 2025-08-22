54de66db9f80d38fe9374ae3023d0f18e6dab4ee
savisalanjala17 <savisalanjala17@gmail.com>
2025-08-22 19:06:04 +0530

    Update and rename 2.java to 02.Reverse String.java

--- LEETCODE/TWO POINTER/02.Reverse String.java
+++ LEETCODE/TWO POINTER/02.Reverse String.java
@@ -23,12 +23,14 @@ class Solution {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+344. Reverse String
 Hint
 Write a function that reverses a string. The input string is given as an array of characters s.
 
 You must do this by modifying the input array in-place with O(1) extra memory.
 
  
+
 Example 1:
 
 Input: s = ["h","e","l","l","o"]
