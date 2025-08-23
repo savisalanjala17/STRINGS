7687c2132c7fff719e4add2dc30efcfd624080c1
savisalanjala17 <savisalanjala17@gmail.com>
2025-08-23 07:57:39 +0530

    Create 04.Goal Parser Interpretation.java

---
+++ LEETCODE/BASIC/04.Goal Parser Interpretation.java
@@ -0,0 +1,69 @@
+class Solution {
+    public String interpret(String command) {
+
+        StringBuilder s= new StringBuilder();
+
+        for(int i=0;i<command.length();i++){
+
+            if(command.charAt(i)=='G'){
+                s.append("G");
+            }
+
+            else if(command.charAt(i)=='(' && command.charAt(i+1)==')'){
+                s.append("o");
+                i++;
+
+            }
+
+            else if(command.charAt(i)=='(' && command.charAt(i+1)=='a'){
+                s.append("al");
+                i+=3;
+            }
+
+
+        
+
+        }
+
+        return new String (s);
+        
+    }
+}
+
+
+1678. Goal Parser Interpretation
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+
+ 
+
+Example 1:
+
+Input: command = "G()(al)"
+Output: "Goal"
+Explanation: The Goal Parser interprets the command as follows:
+G -> G
+() -> o
+(al) -> al
+The final concatenated result is "Goal".
+Example 2:
+
+Input: command = "G()()()()(al)"
+Output: "Gooooal"
+Example 3:
+
+Input: command = "(al)G(al)()()G"
+Output: "alGalooG"
+ 
+
+Constraints:
+
+1 <= command.length <= 100
+command consists of "G", "()", and/or "(al)" in some order.
